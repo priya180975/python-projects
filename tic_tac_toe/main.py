cef183edf2f3e5f15d26187c05dd551f020dc062
Priya <priyannayak100@gmail.com>
2022-10-16 11:23:48 +0530

    tic-tac-toe

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,92 +1,88 @@
-#tic tac toe display 
-#user 
-#computer 
-
 import random
-from turtle import pos
-
 
 print("Tic Tac Toe ")
 
-# values=[' ' for i in range(9)]
+values=[[' ' for i in range(3)] for j in range(3)]
 positions=[i for i in range(9)]
-
-#0-2
-#3-5
-#6-8
 
 def print_struct(values):
     print("\n")
+    for element in values:
+        for i,el in enumerate(element): 
+            if((i+1)%3==0):
+                print('| ',end="")
+                print(el,end="")
+                print(' |',end="\n\n")
+            else:
+                print('| ',end="")
+                print(el,end="")
+                print(' |',end="")
 
-    for i,element in enumerate(values):
-        if((i+1)%3==0):
-            print('| ',end="")
-            print(element,end="")
-            print(' |',end="\n\n")
-        else:
-            print('| ',end="")
-            print(element,end="")
-            print(' |',end="")
+def result(val):
+    winner=None
+    if winner==None:
+        for i in range(3):
+            if (set(values[i]))=={f'{val}'}:
+                return val                
+    
+    if winner==None:                
+        for i in range(3):
+            set_items=set({})
+            for j in range(3):
+                set_items.add(values[j][i])
+            
+            if (set(set_items))=={f'{val}'}:
+                return val
+    
+    if winner==None:                
+        if (values[0][0]==values[1][1]==values[2][2]==val):
+            return val
+        elif (values[0][2]==values[1][1]==values[2][0]==val):
+            return val
+    
+    if not(positions) and winner==None:
+        return "Tie"
 
 def input_user():
     if positions:
         user=int(input("Enter a position : ")) 
 
-        while user not in positions:
-            if user not in range(9): 
-                print("Input should be between 0-8")
+        while user-1 not in positions:
+            if user not in range(1,10): 
+                print("Input should be between 1-9")
             else:
                 print("Position is already used")
             user=int(input("Enter a new position : "))     
         
-        if user in positions:
-            positions.remove(user)
-            values[user]='x'
+        if user-1 in positions:
+            values[(user-1)//3][(user-1)%3]='x'
             print_struct(values)
-
-        print(positions)
-    else:
-        result()
-
+            positions.remove(user-1)
+            res=result('x')
+            if res:
+                if res=='x':
+                    print("Yay! you win")
+                    positions.clear()
+                else:
+                    print(res)
+            else:
+                input_comp()
     
 def input_comp():
     if positions:
         comp=random.choice(positions)
-        positions.remove(comp)
-        values[comp]='o'
+        values[(comp)//3][(comp)%3]='o'
         print_struct(values)
+        positions.remove(comp)
+        res=result('o')
+        if res:
+            if res=='o':
+                print("Computer wins!!")
+                positions.clear()
+            else:
+                print(res)        
 
-        print(positions)
-    else:
-        result()
+print_struct(values)
 
-
-def result():
-    if not(positions):
-        print("Exitt")
-
-
-# print_struct(values)
-# while positions:    
-#     input_user()
-#     input_comp()
-
-
-# values=['x','x','o','x','x','x','x','x','x']
-# res=[]
-# check="x"
-
-# if ((values[0]==values[1]==values[2]==check) or 
-# (values[3]==values[4]==values[5]==check) or 
-# (values[6]==values[7]==values[8]==check)):
-#    print(check)
-
-# elif ((values[0]==values[3]==values[6]==check) or 
-# (values[1]==values[4]==values[7]==check) or 
-# (values[2]==values[5]==values[8]==check)):
-#    print(check)
-
-# elif ((values[0]==values[4]==values[8]==check) or 
-# (values[2]==values[4]==values[6]==check)):
-#    print(check)
-    
+while positions:
+    input_user()
