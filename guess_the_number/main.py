22d0a32d6f6f1e62adf5702edfcad82b681942d3
Priya <priyannayak100@gmail.com>
2022-10-16 13:19:40 +0530

    number guess

--- guess_the_number/main.py
+++ guess_the_number/main.py
@@ -2,6 +2,7 @@
 import random
 val=random.randint(1,10)
 
+print("----------------------\nuser guess")
 def user_guess():
     guess=int(input("Guess a number between 1-10:"))
     while guess!=val:
@@ -14,3 +15,43 @@ def user_guess():
     print(f"Yay!! you have guessed the value {val}")
 
 user_guess()
+
+
+print("----------------------\ncomputer guess")
+print("Think of a value between 0 and 100 ")
+
+def computer_guess():
+    c_guess=random.randint(0,100)
+    judge=input(f"Is {c_guess} high(H), low(L) or correct (C) :")
+    low=0
+    high=100
+    while judge.lower()!='c':
+
+        if low==high:
+            print(f"Your number must be {low}")
+            break
+
+        elif judge.lower()=='l':
+            if c_guess>=high:
+                print("Invalid")
+            else:
+                low=c_guess+1
+                c_guess=random.randint(low,high)
+            judge=input(f"Is {c_guess} high(H), low(L) or correct (C) :")
+        
+        elif judge.lower()=='h':
+            if c_guess<=low:
+                print("Invalid")
+            else:
+                high=c_guess-1
+                c_guess=random.randint(low,high)
+            judge=input(f"Is {c_guess} high(H), low(L) or correct (C) :")
+        
+        else:
+            print("wrong input")
+            judge=input(f"Is {c_guess} high(H), low(L) or correct (C) :")
+    else:
+        print(f"Yay!! computer guessed your number {c_guess}")
+
+computer_guess()
+
